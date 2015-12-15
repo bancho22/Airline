2b46e2d491ad03031de043c07c377202258cddac
Bancho Petrov <banchopetrov@gmail.com>
2015-12-15 12:03:05 +0100

    B

---
+++ src/java/deploy/DeploymentConfiguration.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deploy;
+
+import javax.servlet.annotation.WebListener;
+
+/**
+ *
+ * @author Bancho
+ */
+@WebListener
+public class DeploymentConfiguration {
+    
+    public static String PU_NAME = "Airline_PU"; //USE the RIGHT name here
+    
+}
