19fede83c4b272c1c96bf8abaa08de217193b39c
Alexander <alexandar.g@abv.bg>
2015-12-15 13:36:31 +0200

    1methodAPi

--- src/java/rest/FlightInfoApi.java
+++ src/java/rest/FlightInfoApi.java
@@ -5,14 +5,13 @@
  */
 package rest;
 
+import facade.FlightFacade;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -23,6 +22,7 @@ import javax.ws.rs.core.Response;
  */
 @Path("flightinfo")
 public class FlightInfoApi {
+    FlightFacade ff = new FlightFacade();
 
     @Context
     private UriInfo context;
@@ -31,14 +31,24 @@ public class FlightInfoApi {
     }
 
     @GET
-    @Path("")
+     @Path("api/flightinfo/{from}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getFlights(){
+    public Response getFlights(@PathParam("from") String from, @PathParam("date") String date, @PathParam("tickets") String tickets){
+        
+        
+        
+        
+        
+        
+        
+        
         return null;
+        
+        
+        
     }
     
     @GET
-    @Path("longerPath")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getFlights2(){
         return null;
