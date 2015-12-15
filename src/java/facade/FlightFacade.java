834e72db5595f792007b335e8009c5a3b930fae2
Bancho Petrov <banchopetrov@gmail.com>
2015-12-15 12:12:20 +0100

    C

--- src/java/facade/FlightFacade.java
+++ src/java/facade/FlightFacade.java
@@ -26,4 +26,6 @@ public class FlightFacade {
         return emf.createEntityManager();
     }
     
+    
+    
 }
---
+++ src/java/rest/FlightReservationApi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Bancho
+ */
+@Path("flightreservation")
+public class FlightReservationApi {
+
+    public FlightReservationApi() {
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response makeReservation(String json){
+        return null;
+    }
+
+}
